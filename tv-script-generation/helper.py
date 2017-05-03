2e11c9b640a04abdbdf8958e66cc7dc9488bfae8
shadowinder <gcc1117@gmail.com>
2017-05-03 09:58:16 +0800

    p4 init

--- tv-script-generation/helper.py
+++ tv-script-generation/helper.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import codecs
 
 
 def load_data(path):
@@ -7,7 +8,7 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, "r") as f:
+    with codecs.open(input_file, "r",encoding='utf8') as f:
         data = f.read()
 
     return data
